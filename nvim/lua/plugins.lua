6271ad20317d864b4270a5519053f6bc2ec2167d
Kirill Mokevnin <mokevnin@gmail.com>
2022-01-03 02:07:25 -0500

    improve plugins
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -9,6 +9,10 @@ function(use)
       npairs.add_rules(require('nvim-autopairs.rules.endwise-ruby'))
       npairs.add_rules(require('nvim-autopairs.rules.endwise-elixir'))
       npairs.add_rules(require('nvim-autopairs.rules.endwise-lua'))
+
+      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+      local cmp = require('cmp')
+      cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
     end
   }
   -- use 'tpope/vim-sleuth'
