05a33148deab127d4cd034fd156dc8fad4fb1f86
Kirill Mokevnin <mokevnin@gmail.com>
2022-10-16 08:32:35 -0400

    update nvim configuration
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -53,6 +53,7 @@ local options = {
 local globals = {
   mapleader = ' ', --- Map leader key to SPC
   loaded = 1,
+  loaded_netrw = 1,
   loaded_netrwPlugin = 1,
   -- speeddating_no_mappings     = 1,          --- Disable default mappings for speeddating
 }
