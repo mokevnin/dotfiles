7f218e96494d52941a0829a843c9115f8b3826c0
Kirill Mokevnin <mokevnin@gmail.com>
2022-11-10 21:08:51 -0500

    improve Makefile
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- install.sh
+++ install.sh
@@ -4,14 +4,15 @@ case "$OSTYPE" in
   linux*)
     sudo add-apt-repository -y ppa:neovim-ppa/stable
     sudo apt-get update
-    sudo apt-get install neovim
-    sudo apt install -yy zsh git
+    apt install -yy zsh git neovim make neovim git \
+      silversearcher-ag fd-find fzf bat htop ncdu \
+      tldr httpie exuberant-ctags zip
+
     chsh -s /bin/zsh
 
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2 || exit 0
-    echo ". $$HOME/.asdf/asdf.sh" > ~/.oh-my-zsh/custom/asdf.zsh
-    apt install -yy make neovim git silversearcher-ag fd-find fzf bat htop ncdu tldr httpie exuberant-ctags zsh zip
+    echo ". \$HOME/.asdf/asdf.sh" > ~/.oh-my-zsh/custom/asdf.zsh
     ;;
   darwin*)
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
