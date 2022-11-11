4d784f747034a565d673dc2778d8bab26b4bbca3
Kirill Mokevnin <mokevnin@gmail.com>
2022-11-10 21:20:34 -0500

    improve Makefile
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- install.sh
+++ install.sh
@@ -6,13 +6,16 @@ case "$OSTYPE" in
     sudo apt-get update
     apt install -yy zsh git neovim make neovim git \
       silversearcher-ag fd-find fzf bat htop ncdu \
-      tldr httpie exuberant-ctags zip
+      tldr httpie exuberant-ctags zip build-essential
 
     chsh -s /bin/zsh
 
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" || true
     git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2 || true
     echo ". \$HOME/.asdf/asdf.sh" > ~/.oh-my-zsh/custom/asdf.zsh
+
+    # https://github.com/rbenv/ruby-build/wiki#suggested-build-environment
+    apt install -yy autoconf bison patch build-essential rustc libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev
     ;;
   darwin*)
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -25,7 +28,7 @@ esac
 
 git clone https://github.com/mokevnin/dotfiles || true
 
-$HOME/.asdf/asdf.sh
+sh "$HOME/.asdf/asdf.sh"
 
 asdf plugin add nodejs
 asdf plugin add php
