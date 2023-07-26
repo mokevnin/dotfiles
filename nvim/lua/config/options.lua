aa7b18849eeb724c1b3f9af1f2e6ffae96fa3802
Kirill Mokevnin <mokevnin@gmail.com>
2023-07-26 19:46:07 -0400

    update options
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,5 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.o.langmap =
+	"ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz"
+
 vim.o.relativenumber = false
-vim.o.spelllang="ru_ru,en_us"
+vim.o.spelllang = "ru_ru,en_us"
+vim.o.linebreak = true
