32c59d457c7cd2f34289f38e2645436b316cfa6a
Kirill Mokevnin <mokevnin@gmail.com>
2023-09-05 13:04:44 -0400

    simplify java configuration
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -4,15 +4,16 @@ return {
     opts = function(_, opts)
       table.insert(opts.ensure_installed, "kotlin-language-server")
       table.insert(opts.ensure_installed, "lemminx")
+      table.insert(opts.ensure_installed, "google-java-format")
     end,
   },
   -- {
-  -- 	"jose-elias-alvarez/null-ls.nvim",
-  -- 	opts = function(_, opts)
-  -- 		local nls = require("null-ls")
-  -- 		opts.sources = vim.list_extend(opts.sources, {
-  -- 			nls.builtins.diagnostics.write_good,
-  -- 		})
-  -- 	end,
+  --   "jose-elias-alvarez/null-ls.nvim",
+  --   opts = function(_, opts)
+  --     local nls = require("null-ls")
+  --     opts.sources = vim.list_extend(opts.sources, {
+  --       nls.builtins.formatting.google_java_format,
+  --     })
+  --   end,
   -- },
 }
