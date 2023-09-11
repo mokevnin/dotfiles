df4fcaa608ee104a1c8688cacde4480e75c09bbd
Kirill Mokevnin <mokevnin@gmail.com>
2023-09-11 13:42:39 -0400

    update nvim configuration
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- nvim/lua/plugins/others.lua
+++ nvim/lua/plugins/others.lua
@@ -22,7 +22,9 @@ return {
   },
   {
     "iamcco/markdown-preview.nvim",
-    config = function()
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
       vim.fn["mkdp#util#install"]()
     end,
   },
