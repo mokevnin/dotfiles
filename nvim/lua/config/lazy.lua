7d7be5137ac82b7ba9627f29bc0e458d94fe36a0
Kirill Mokevnin <mokevnin@gmail.com>
2024-01-26 18:29:51 -0500

    update
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -10,7 +10,7 @@ require('lazy').setup({
   change_detection = { notify = false },
   spec = {
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    -- { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- { import = "lazyvim.plugins.extras.linting.eslint" },
     -- { import = "lazyvim.plugins.extras.lsp.none-ls" },
     { import = "lazyvim.plugins.extras.dap.core" },
