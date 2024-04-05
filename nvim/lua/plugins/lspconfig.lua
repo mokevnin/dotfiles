359eb717820569a2fe5ccaa784339251bf9e53ac
Kirill Mokevnin <mokevnin@gmail.com>
2024-04-04 23:07:52 -0400

    enable inlay hints
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -2,22 +2,52 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      inlay_hints = {
+        enabled = true,
+      },
+      codelens = {
+        enabled = false,
+      },
       diagnostics = {
-        underline = true,
-        virtual_text = false,
-        float = {
-          border = "rounded",
-        },
+        -- underline = true,
+        -- virtual_text = true,
+        -- float = {
+        --   border = "rounded",
+        -- },
       },
       servers = {
         kotlin_language_server = {},
+        -- https://github.com/typescript-language-server/typescript-language-server/pull/218
+        -- https://github.com/microsoft/TypeScript/issues/13270
+        tsserver = {
+          settings = {
+            tsserver = {
+              -- codeLens = {
+              --   enable = true,
+              -- },
+              init_options = {
+                preferences = {
+                  quoteStyle = "single",
+                  includeInlayParameterNameHints = "all",
+                  includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                  includeInlayFunctionParameterTypeHints = true,
+                  includeInlayVariableTypeHints = true,
+                  includeInlayPropertyDeclarationTypeHints = true,
+                  includeInlayFunctionLikeReturnTypeHints = true,
+                  includeInlayEnumMemberValueHints = true,
+                  importModuleSpecifierPreference = "non-relative",
+                },
+              },
+            },
+          },
+        },
         html = {
-          filetypes = { 'html', 'blade' }
+          filetypes = { "html", "blade" },
         },
         bashls = {},
         lemminx = {},
         phpactor = {
-          filetypes = { 'php', 'blade' }
+          filetypes = { "php", "blade" },
         },
         ltex = {
           settings = {
